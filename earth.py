46b16fb5ebd31d8d1dbb0d4e2705b7c074f76c1e
VicenteLeone <vleone@mail.austral.edu.ar>
2025-03-28 12:20:52 -0300

    Update earth.py

--- earth.py
+++ earth.py
@@ -1,3 +1,15 @@
 def earth():
     x = "Bangladesh"
     y = "Barbados"
+    
+    result1 = x < y
+    result2 = y < x
+    
+    return [
+        f"The result of {x} comes first in the dictionary than {y} is {result1}.",
+        f"The result of {y} comes first in the dictionary than {x} is {result2}."
+    ]
+
+if __name__ == "__main__":
+    for line in earth():
+        print(line)
