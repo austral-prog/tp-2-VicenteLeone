04f89565dc8471fe84d41d8174b4e09a241f88e0
VicenteLeone <vleone@mail.austral.edu.ar>
2025-03-28 12:37:42 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,16 @@
 def change():
     expense = 23.75
     money = 100
+    
+    vuelto = money - expense
+    pesos = int(vuelto)
+    centavos = int(round((vuelto - pesos) * 100))
+    
+    print("\nVuelto\n")
+    print("Pesos:")
+    print(f"{pesos}")
+    print("Centavos:")
+    print(f"{centavos}")
+
+if __name__ == "__main__":
+    change()
