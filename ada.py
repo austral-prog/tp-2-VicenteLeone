6d7576581e52fe685ec115c7e7d6bcfc0430200d
VicenteLeone <vleone@mail.austral.edu.ar>
2025-03-28 12:18:46 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,13 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
-    return [
-        f"{first_name.lower()} {last_name.lower()}",
-        f"{first_name.capitalize()} {last_name.capitalize()}",
-        f"{first_name.upper()} {last_name.upper()}",
-        f"\t{first_name.lower()} {last_name.lower()}"
-    ]
+    print(f"\t{first_name.lower()} {last_name.lower()}")
+    print(f"\t{first_name.title()} {last_name.title()}")
+    print(f"\t{first_name.upper()} {last_name.upper()}")
 
-if __name__ == "__main__":
-    for line in ada():
-        print(line)
+ada() 
