0ad7c0de44f49f23d6836149832791b93109bf32
VicenteLeone <vleone@mail.austral.edu.ar>
2025-03-28 11:57:36 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,17 +1,4 @@
-def format_names(first_name: str, last_name: str) -> list:
-    """
-    Given a first name and a last name, return a list with the formatted names in different styles.
-    """
-    return [
-        f"{first_name.lower()} {last_name.lower()}",
-        f"{first_name.capitalize()} {last_name.capitalize()}",
-        f"{first_name.upper()} {last_name.upper()}",
-        f"\t{first_name.lower()} {last_name.lower()}"
-    ]
-
-
-if __name__ == "__main__":
-    first_name = "AdA"
-    last_name = "LoVeLAce"
-    for line in format_names(first_name, last_name):
-        print(line)
+def ada(): 
+    first_name = "AdA" 
+    last_name = "loVeLAce" 
+    
